a0589a2391aa7097769f5967ac97fe1eecb8ba40
rifa <rifadayyan@gmail.com>
2026-07-17 22:48:45 +0700

    fix menu

--- resources/views/base/base-dash-index.blade.php
+++ resources/views/base/base-dash-index.blade.php
@@ -5,10 +5,10 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>@yield('menu') - {{ $web->school_name }}</title>
         @php
             $web = \App\Models\Settings\WebSettings::where('id', 1)->first();
         @endphp
+        <title>@yield('menu') - {{ $web->school_name }}</title>
 
         @include('base.panel.base-panel-header-script')
 
