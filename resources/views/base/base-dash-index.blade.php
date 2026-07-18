828bb86f120dd37ccc5ebd7228ed0dc05cfdb4ab
rifa <rifadayyan@gmail.com>
2026-07-18 11:46:21 +0700

    fix it

--- resources/views/base/base-dash-index.blade.php
+++ resources/views/base/base-dash-index.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         @php
-            $web = \App\Models\Settings\WebSettings::where('id', 1)->first();
+            $web = \App\Models\Settings\webSettings::where('id', 1)->first();
         @endphp
         <title>@yield('menu') - {{ $web->school_name }}</title>
 
